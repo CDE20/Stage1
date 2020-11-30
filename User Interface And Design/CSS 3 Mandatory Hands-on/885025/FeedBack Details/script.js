2a3cbdd4adc81ecfb71f4bc7b7a6005150bdac3f
IJ006 <75236474+IJ006@users.noreply.github.com>
2020-11-30 16:40:33 +0530

    Add files via upload

---
+++ User Interface And Design/CSS 3 Mandatory Hands-on/885025/FeedBack Details/script.js
@@ -0,0 +1,20 @@
+var feedlist = [];
+function addFeedback(){
+ //Fill the required logic   
+ var feed = document.getElementById("feedback").value;
+ feedlist.push(feed);
+ document.getElementById("feedback").value = "";
+ 
+ document.getElementById("result").innerHTML = "<h2> Feedback Details</h2><br><p>Successfully Added Feedback Details</p>"
+
+}
+
+function displayFeedback(){
+    //Fill the required logic
+    var display = "";
+    var count=0;
+    for (var i in feedlist){
+        display += "Feedback "+(++count) + " <br>" + feedlist[i] + " <br>"; 
+    }
+    document.getElementById("result").innerHTML = "<h2> Feedback Details</h2>" + display;
+}
