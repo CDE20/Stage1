13513306a5360d49ed1ed9cc820c94f0a9e8a5c4
IJ006 <75236474+IJ006@users.noreply.github.com>
2020-12-04 09:27:27 +0530

    Add files via upload

---
+++ User Interface And Design/TruYum Case Study/885108/truyum-ui/js/script.js
@@ -0,0 +1,11 @@
+
+function addToCart()
+{
+	 document.getElementById("result").style.display='block';
+	document.getElementById("result").innerHTML="Item Added To Cart Successfully";
+}
+function goto_()
+{
+     window.open('edit-menu-item-status.html','_self');
+      return false;
+}
