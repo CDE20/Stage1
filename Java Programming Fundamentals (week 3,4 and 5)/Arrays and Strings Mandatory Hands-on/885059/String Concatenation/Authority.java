e11954fd4e07f127b41528efecd188d56b6bd8a6
IJ006 <75236474+IJ006@users.noreply.github.com>
2020-12-09 12:20:37 +0530

    Add files via upload

---
+++ Java Programming Fundamentals (week 3,4 and 5)/Arrays and Strings Mandatory Hands-on/885059/String Concatenation/Authority.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Authority {
+    public static void main (String[] args) {
+        String resident, father;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inmate's name:");
+        resident = sc.nextLine();
+        System.out.println("Inmate's father's name:");
+        father = sc.nextLine();
+        char[] r = resident.toCharArray();
+        char[] f = father.toCharArray();
+        if(check(r) || check(f))
+        {
+            System.out.println("Invalid name");
+        }
+        else
+        {
+            System.out.println(resident.toUpperCase().concat(" ").concat(father.toUpperCase()));
+        }
+    }
+    public static boolean check(char[] s)
+    {
+        for(char c: s)
+        {
+            if(c == ' ')
+                continue;
+            if(!Character.isLetter(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
