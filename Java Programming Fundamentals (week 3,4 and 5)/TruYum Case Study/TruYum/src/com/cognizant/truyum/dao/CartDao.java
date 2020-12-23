6e939e76d312b6cdee48e318e25dd18f31792b3e
NehaShilamkar <75242512+NehaShilamkar@users.noreply.github.com>
2020-12-23 21:21:01 +0530

    Add files via upload

---
+++ Java Programming Fundamentals (week 3,4 and 5)/TruYum Case Study/TruYum/src/com/cognizant/truyum/dao/CartDao.java
@@ -0,0 +1,18 @@
+package com.cognizant.truyum.dao;
+
+import java.util.List;
+
+import com.cognizant.truyum.model.MenuItem;
+
+/**
+ * 
+ * @author 
+ *
+ */
+public interface CartDao {
+	void addCartItem(long userId, long menuItemId);
+
+	public List<MenuItem> getAllCartItems(long userId) throws CartEmptyException;
+
+	void removeCartItem(long userId,long menuItemId);
+}
