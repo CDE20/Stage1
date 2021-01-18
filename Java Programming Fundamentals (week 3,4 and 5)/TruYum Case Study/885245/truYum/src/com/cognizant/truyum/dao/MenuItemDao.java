e56aadc1c354bac7484312cdf33260c7030d2762
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-01-18 10:44:27 +0530

    Add files via upload

---
+++ Java Programming Fundamentals (week 3,4 and 5)/TruYum Case Study/885245/truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
@@ -0,0 +1,21 @@
+package com.cognizant.truyum.dao;
+
+import java.util.List;
+
+import com.cognizant.truyum.model.MenuItem;
+
+/**
+ * 
+ * @author 
+ *
+ */
+public interface MenuItemDao {
+	List<MenuItem> getMenuItemListAdmin();
+
+	List<MenuItem> getMenuItemListCustomer();
+
+	void modifyMenuItem(MenuItem menuItem);
+
+	MenuItem getMenuItem(long menuItemId);
+
+}
